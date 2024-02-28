e41a9ad3e6521ca263a8e18a3db627e515ee4ed6
Lavanya Dileep <lavanyadi@infinite.com>
2024-02-28 12:12:36 +0530

    test1

---
+++ Test1/codetest1/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TEST1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a string: ");
+            string input = Console.ReadLine();
+
+            Console.Write("enter d position of d char to be removed : ");
+            int position = int.Parse(Console.ReadLine());
+
+            if (position < 0 || position >= input.Length)
+            {
+                Console.WriteLine("Invalid position....");
+            }
+            else
+            {
+                string result = RemoveChar(input, position);
+                Console.WriteLine($"Result is : {result}");
+                Console.ReadKey();
+            }
+        }
+
+        static string RemoveChar(string input, int position)
+        {
+            string result = " ";
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (i != position)
+                {
+                    result += input[i];
+                }
+            }
+            return result;
+            
+        }
+    }
+}
---
+++ Test1/codetest3/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace codetest3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+                Console.WriteLine("enter 3 integers:");
+                int n1 = Convert.ToInt32(Console.ReadLine());
+                int n2 = Convert.ToInt32(Console.ReadLine());
+                int n3 = Convert.ToInt32(Console.ReadLine());
+
+                int l = larg(n1, n2, n3);
+
+                Console.WriteLine($"largest number is :{l}");
+                Console.ReadLine();
+        }
+
+            static int larg(int n1, int n2, int n3)
+            {
+                if (n1 >= n2 && n1 >= n3)
+                {
+                    return n1;
+                }
+                else if (n2 >= n1 && n2 >= n3)
+                {
+                    return n2;
+                }
+                else
+                {
+                    return n3;
+                }
+            }
+    }
+
+}
+
