cc6c66997a7135bad8d2c7d12e5baceeec15e3b5
Lavanya Dileep <lavanyadi@infinite.com>
2024-03-03 14:23:33 +0530

    assignmnet

---
+++ Assignment_1/Q1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter n1 value");
+            int n1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("enter n2 value");
+            int n2 = Convert.ToInt32(Console.ReadLine());
+
+            if (n1 == n2)
+            {
+                Console.WriteLine($"{n1} and {n2} are same");
+            }
+            else
+            {
+                Console.WriteLine("they are not same");
+            }
+            Console.ReadKey();
+        }
+    }
+}
