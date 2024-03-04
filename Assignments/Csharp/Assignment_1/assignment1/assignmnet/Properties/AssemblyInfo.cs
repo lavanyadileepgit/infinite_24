f5cb41f0cb7c410fc22480a291a24af569736995
Lavanya Dileep <lavanyadi@infinite.com>
2024-03-04 11:58:14 +0530

    assesment

---
+++ Assignments/Csharp/Assignment_1/assignment1/assignmnet/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("assignmnet")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("assignmnet")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("978ee4a8-3fb0-413a-9290-2f849f8020af")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Assignments/Csharp/Assignment_1/assignment1/assignmnet/strings.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace assignmnet
+{
+    class strings
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter a word");
+            string str = Console.ReadLine();
+            int n = str.Length;
+            Console.WriteLine($"word length is {n} and word is {str}");
+            Console.ReadLine();
+
+
+            Console.WriteLine("enter string");
+            string s = Console.ReadLine();
+            string e = "";
+            for (int i = s.Length - 1; i >= 0; i--)
+            {
+                e += s[i];
+            }
+            Console.WriteLine(e);
+            Console.ReadLine();
+
+            Console.WriteLine("enter first string");
+            string s1 = Console.ReadLine();
+            Console.WriteLine("enter second string");
+            string s2 = Console.ReadLine();
+            Console.WriteLine(s1 == s2 ? "same" : "different");
+            Console.ReadLine();
+        }
+    }
+
+}
