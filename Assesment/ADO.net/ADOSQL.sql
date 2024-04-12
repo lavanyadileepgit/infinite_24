dd3abf3674f289cb9e4fc0fdc491c1293b396e5a
Lavanya Dileep <lavanyadi@infinite.com>
2024-04-12 11:19:02 +0530

    ado

---
+++ Assesment/ADO.net/ADOSQL.sql
@@ -0,0 +1,34 @@
+create database employeemanagement
+use employeemanagement
+
+CREATE TABLE Employee_Details
+(
+    Empno int primary key,
+    EmpName varchar(20) NOT NULL,
+    Empsal float CHECK (Empsal >= 25000),
+    Emptype char(1) CHECK (Emptype IN ('P', 'C'))
+)
+
+select* from employee_details
+delete from Employee_Details where empno=103
+
+CREATE OR ALTER PROCEDURE addemployee
+(
+    @name VARCHAR(20),
+    @empsal DECIMAL,
+    @emptype CHAR(1)
+)
+AS
+BEGIN
+    DECLARE @empno INT;
+    SELECT @empno = ISNULL(MAX(Empno), 99) + 1 FROM Employee_Details;
+    INSERT INTO Employee_Details (Empno, EmpName, Empsal, Emptype)
+    VALUES (@empno, @name, @empsal, @emptype);
+END
+
+exec addemployee 'Lavanya', 50000.00 , 'P'
+
+----------------------------------------------------------------------------------------
+--100	Lavanya Dileep	50000	P
+--101	Ramya Shree	    30000	C
+--102	Hema	        40000	P
---
+++ Assesment/ADO.net/employee/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace employee
+{
+    class Program
+        {
+            static void Main()
+            {
+                string connectionString = "Data Source=ICS-LT-FQW1ZD3;Initial Catalog=employeemanagement;Integrated Security=True"; 
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+
+                    using (SqlCommand command = new SqlCommand("addemployee", connection))
+                    {
+                        command.CommandType = System.Data.CommandType.StoredProcedure;
+
+                        Console.Write("Enter name: ");
+                        string Name = Console.ReadLine();
+                        Console.Write("Enter salary: ");
+                        string salary = Console.ReadLine();
+                        Console.Write("Enter employee type: ");
+                        string type = Console.ReadLine();
+
+                        command.Parameters.AddWithValue("@name", Name);
+                        command.Parameters.AddWithValue("@empsal", salary);
+                        command.Parameters.AddWithValue("@emptype",type );
+
+                        command.ExecuteNonQuery();
+                        Console.WriteLine("Employee added successfully.");
+                    }
+
+                    connection.Close();
+                }
+
+            
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+
+                using (SqlCommand command = new SqlCommand("SELECT * FROM Employee_Details", connection))
+                using (SqlDataReader reader = command.ExecuteReader())
+                {
+                    Console.WriteLine("Empno\tEmpName\t\tEmpsal\tEmptype");
+                    while (reader.Read())
+                    {
+                        Console.WriteLine(reader["Empno"] + "\t" + reader["EmpName"] + "\t" + reader["Empsal"] + "\t" + reader["Emptype"]);
+                    }
+                }
+
+                connection.Close();
+            }
+
+
+            Console.ReadLine(); 
+            }
+        }
+    }
+
+
